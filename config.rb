43f9255b69a37ab0ec55ce6c7b58d5c83d0fe50d
Rob Sterlini <sterlini@fueled.com>
2015-03-01 22:09:14 +0000

    Forked robsterlini.co.uk
    Signed-off-by: Rob Sterlini <sterlini@fueled.com>

---
+++ config.rb
@@ -0,0 +1,114 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+### t
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Requires
+require 'builder'
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+String.class_eval do
+  def to_slug
+    value = self.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').to_s
+    value.gsub!(/[']+/, '')
+    value.gsub!(/\W+/, ' ')
+    value.strip!
+    value.downcase!
+    value.gsub!(' ', '-')
+    value
+  end
+end
+
+# Set directories
+
+set :css_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/images'
+set :url_root, 'https://robsterlini.co.uk'
+
+# Create pages
+data.projects.featured.each_with_index do |c, i|
+  if c[:case_study] == 'true'
+    proxy "/case-study/#{c[:slug]}.html", "/templates/case-study/case-study.html", :locals => {:project => c, :case_study => c.case_study_vars, :index => i}, :ignore => true
+  end
+end
+
+# Turn on Pretty URLs
+activate :directory_indexes
+
+# Autoprefix this badboy
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 9']
+end
+
+
+# Turn on sitemap
+activate :search_engine_sitemap
+
+# disable layout
+page ".htaccess.apache", :layout => false
+page "redirects/.htaccess.apache", :layout => false
+page "feed.xml", :layout => false
+
+# rename file after build
+after_build do
+  File.rename 'build/.htaccess.apache', 'build/.htaccess'
+end
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Minify HTML
+  activate :minify_html, remove_http_protocol: false
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
---
+++ source/assets/js/main.js
@@ -0,0 +1,17 @@
+//= require _modernizr
+//= require _g
+//= require _plugins
+//= require _browserSniff
+//= require _loadAnimation
+//= require _codeHighlight
+//= require _loadImages
+//= require _navToggle
+//= require _parallax
+//= require _anchorScroll
+//= require _loadHero
+//= require _caseStudyHotspur
+
+$(function() {
+	// This is all we need in our main.js file - isn't it neat :)
+	g._init();
+});
